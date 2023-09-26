76308ecac3f359e1d275170da6a40a55dc246faf
PauFusco <99730051+PauFusco@users.noreply.github.com>
2023-09-26 15:56:05 +0200

    patrol comments

--- Assets/Patrol.cs
+++ Assets/Patrol.cs
@@ -13,6 +13,9 @@ public class Patrol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // use objects as targets and alternate between them
+        // put objects in a list and assign numbers to make it as a loop of (int i = 0; i < n; i++;) where n is max target attributed number
+        // if (current position == target && target != n) -> target = next target
+        // 
     }
 }
