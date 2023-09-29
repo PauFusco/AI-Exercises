9a7d4a14ada46a081e777096e320348e8dfb9f9f
PauFusco <99730051+PauFusco@users.noreply.github.com>
2023-09-29 10:28:09 +0200

    wander updated

--- Assets/Wander.cs
+++ Assets/Wander.cs
@@ -6,38 +6,27 @@ public class Wander : MonoBehaviour
 {
     
     // parameters: float radius, offset;
-    float radius = 2;
+    float radius = 1;
     float offset = 1;
     
     public UnityEngine.AI.NavMeshAgent agent;
     
     UnityEngine.AI.NavMeshHit hit;
-    Vector3 localTarget;
+    Vector3 tempTarget;
     Vector3 worldTarget;
-    
-    int i;
     
     // Update is called once per frame
     void Update()
     {
-        localTarget = UnityEngine.Random.insideUnitCircle * radius;
-        localTarget += new Vector3(0, 0, offset);
-        worldTarget = transform.TransformPoint(localTarget);
-        worldTarget.y = 0f;
-        
-        int i = 0;
-        
-        while (!UnityEngine.AI.NavMesh.SamplePosition(worldTarget, out hit, 1.0f, UnityEngine.AI.NavMesh.AllAreas) && i < 10)
+        for (int i = 0; i < 10; i++)
         {
-            localTarget = UnityEngine.Random.insideUnitCircle * radius;
-            localTarget += new Vector3(0, 0, offset);
-            worldTarget = transform.TransformPoint(localTarget);
+            tempTarget = UnityEngine.Random.insideUnitCircle * radius;
+            tempTarget += new Vector3(0, 0, offset);
+            worldTarget = transform.TransformPoint(tempTarget);
             worldTarget.y = 0f;
-            i++;
+            if (UnityEngine.AI.NavMesh.SamplePosition(worldTarget, out hit, 1.0f, UnityEngine.AI.NavMesh.AllAreas)) break;
         }
-    
-        agent.destination = worldTarget;
 
-        // Debug.DrawRay(worldTarget, Vector3.up, Color.blue, 1.0f);
+        agent.destination = worldTarget;
     }
 }
